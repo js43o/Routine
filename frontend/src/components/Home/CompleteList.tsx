b6a5c75a1fbac7a7afcff9afdba9a59dd35851ac
js43o <js43o@naver.com>
2022-12-26 14:55:27 +0900

    fix: change view of CompleteList

--- frontend/src/components/Home/CompleteList.tsx
+++ frontend/src/components/Home/CompleteList.tsx
@@ -9,21 +9,24 @@ const CompleteListBlock = styled.div<{ visible: boolean }>`
   gap: 0.25rem;
   position: absolute;
   z-index: 10;
-  transform: translateY(2rem);
+  max-height: 160px;
   padding: 0.5rem;
+  border: 1px solid ${({ theme }) => theme.border_main};
   border-radius: 0.5rem;
-  max-width: 50%;
-  max-height: 480px;
   background: ${({ theme }) => theme.background_main};
   color: ${({ theme }) => theme.letter_main};
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
   opacity: ${({ visible }) => (visible ? 1 : 0)};
+  transform: translateY(2rem);
   & > .date {
     font-weight: bold;
     padding-bottom: 0.25rem;
     border-bottom: 1px solid ${({ theme }) => theme.border_main};
   }
   & > .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     font-weight: normal;
     font-size: 0.85rem;
     overflow-y: scroll;
@@ -40,7 +43,9 @@ const CompleteList = ({ complete, visible }: CompleteListProps) => {
     <CompleteListBlock visible={!!(complete && visible)}>
       <div className="date">{complete?.date}</div>
       <div className="content">
-        {complete?.list.map((exer) => exer.exercise)}
+        {complete?.list.map((exer) => (
+          <span>{exer.exercise}</span>
+        ))}
       </div>
     </CompleteListBlock>
   );
